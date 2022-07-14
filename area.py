0020fc2c645075c4d7a10e210a5878fed848d2b1
Akshansh Mathur <109062912+YashaswiMathur@users.noreply.github.com>
2022-07-14 23:43:32 +0530

    Update area.py

--- area.py
+++ area.py
@@ -1,3 +1,8 @@
+#TASK 1: Print area of circle
 r = float(input("enter radius = "))
 Area = 3.14*(r*r)
 print(Area)
+
+#TASK2: Print Filename
+x = input("Input The Filename: ")
+print("The extention of the file is : ")
